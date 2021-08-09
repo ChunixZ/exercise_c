49edbdca327a494a668cb683514c6e25cdd80ab8
chunix@foxmail.com <yunchun>
2021-08-10 00:36:48 +0800

    142

---
+++ leetcode/all/142.环形链表-ii.c
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=c
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *slow = head, *fast = head;
+
+    while (fast != NULL) {
+        slow = slow->next;
+
+        if (fast->next == NULL)
+            return NULL;
+
+        fast = fast->next->next;
+
+        if (fast == slow) {
+            struct ListNode *ptr = head;
+
+            while (ptr != slow) {
+                ptr = ptr->next;
+                slow = slow->next;
+            }
+
+            return ptr;
+        }
+    }
+
+    return NULL;
+}
+// @lc code=end
+
